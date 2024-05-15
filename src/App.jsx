066081afb1dad2852b1a0198690349e9dade1def
unknown <cmarcos@americatv.pe>
2024-05-14 21:28:02 -0600

    primeroscambios

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <p className="font-bold">Hola Mundo</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './tailwind.css';  // Añade esta línea
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+)
