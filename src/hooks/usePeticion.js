f14ac0fa7e494b600dcdc8bae4a26a3de11cfd15
unknown <cmarcos@americatv.pe>
2024-05-15 15:51:02 -0600

    fix_cambios

--- src/hooks/usePeticion.js
+++ src/hooks/usePeticion.js
@@ -1,5 +1,22 @@
-const usePetition = () => {
-    return
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const usePetition = (endpoint) => {
+
+    const API_ULR = import.meta.env.VITE_API_URL
+
+    const [data, setData] = useState({})
+    useEffect(() => {
+        axios.get(`${API_ULR}${endpoint}`)
+            .then((response) => {
+                setData(response.data.data);
+            })
+            .catch(() => {
+                console.log("error")
+            });
+    }, []);
+    
+    return data
 }
 
 export default usePetition
